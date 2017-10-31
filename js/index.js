2dc7b45609d11367a849784263fa534057d7bb2a
Pawel Klasa <pawel.klasa@MAC000170.local>
2017-10-31 13:54:35 +0000

    get button print someting within a div

--- js/index.js
+++ js/index.js
@@ -1,18 +1,36 @@
-var txt = "";
-var items = 
-	{
-		name: 'Pawel',
-		surname: 'Klasa'
-	};
+var entry = [
+  {
+	  name: 'Pawel',
+	  surname: 'Klasa',
+	  number: '07816406630',
+	  addres: '111 Ebbett Court, W3 6BX'
+  },
+  {
+	name: 'Pawel',
+	surname: 'Klasa',
+	number: '07816406630',
+	addres: '111 Ebbett Court, W3 6BX'
+  },
+  {
+	  name: 'Jan',
+	  surname: 'Klasa',
+	  number: '0449598212476',
+	  addres: 'Nozyno 60, 77-115'
+  }
+];
 
+ var handlers = {
+ 	printName: function() {
+ 		var result = document.getElementById("name");
+		//result = entry[0].name;
+		result.innerHTML = entry[0].name;
+		console.log(result.innerHTML);
+    },
+	printSurname: function() {
+ 		var result = document.getElementById("surname");
+		//result = entry[0].name;
+		result.innerHTML = entry[0].surname;
+		console.log(result.innerHTML);
 
-var handlers = {
-	action: function() {
-  document.getElementById("field").innerHTML = items;
-	},
-	print: function() {
-		for (var x in items){
-    document.getElementById("field").innerHTML = txt += items[x];
-		}; 
-	 }
+    }
 };
